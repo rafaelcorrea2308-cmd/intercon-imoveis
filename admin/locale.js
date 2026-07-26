62566d59619035e168703951fc527a6b57e8ed57
rafaelcorrea2308-cmd <rafaelcorrea2308@gmail.com>
2026-07-25 20:02:01 -0400

    Add locale.js for Portuguese language support

---
+++ admin/locale.js
@@ -0,0 +1,19 @@
+(function () {
+  function waitForCMS() {
+    if (!window.CMS) {
+      setTimeout(waitForCMS, 50);
+      return;
+    }
+
+    import('https://unpkg.com/decap-cms-locales@3.8.0/dist/esm/pt/index.js')
+      .then(function (mod) {
+        var pt = (mod && mod.default) ? mod.default : mod;
+        window.CMS.registerLocale('pt', pt);
+      })
+      .catch(function (e) {
+        console.error('Nao foi possivel carregar o idioma pt do painel', e);
+      });
+  }
+
+  waitForCMS();
+})();
