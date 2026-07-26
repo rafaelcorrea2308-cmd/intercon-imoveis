e8fe164a016a1e1168905d8d0a8f92998e6f9bbd
rafaelcorrea2308-cmd <rafaelcorrea2308@gmail.com>
2026-07-25 20:55:53 -0400

    Chamar CMS.init() apos registrar locale pt (fix idioma painel)
    Usa CMS_MANUAL_INIT (definido em index.html) e chama window.CMS.init() apos o import do locale pt, para que a interface realmente carregue traduzida.

--- admin/locale.js
+++ admin/locale.js
@@ -1,19 +1,22 @@
 (function () {
-  function waitForCMS() {
-    if (!window.CMS) {
-      setTimeout(waitForCMS, 50);
-      return;
+    function waitForCMS() {
+          if (!window.CMS) {
+                  setTimeout(waitForCMS, 50);
+                  return;
+          }
+
+      import('https://unpkg.com/decap-cms-locales@3.8.0/dist/esm/pt/index.js')
+            .then(function (mod) {
+                      var pt = (mod && mod.default) ? mod.default : mod;
+                      window.CMS.registerLocale('pt', pt);
+            })
+            .catch(function (e) {
+                      console.error('Nao foi possivel carregar o idioma pt do painel', e);
+            })
+            .finally(function () {
+                      window.CMS.init();
+            });
     }
 
-    import('https://unpkg.com/decap-cms-locales@3.8.0/dist/esm/pt/index.js')
-      .then(function (mod) {
-        var pt = (mod && mod.default) ? mod.default : mod;
-        window.CMS.registerLocale('pt', pt);
-      })
-      .catch(function (e) {
-        console.error('Nao foi possivel carregar o idioma pt do painel', e);
-      });
-  }
-
-  waitForCMS();
+   waitForCMS();
 })();
